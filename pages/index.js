5859672251bbc39fecbb003fbb153a2a85dee515
ehmaddd <ehmaddd@gmail.com>
2023-10-23 22:53:37 +0500

    Show items and adjust sizing

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,73 @@
+import Carousel from 'react-multi-carousel';
+import 'bootstrap/dist/css/bootstrap.css';
 import React, { useEffect, useState } from 'react';
+import 'react-multi-carousel/lib/styles.css';
+import './MultiItemCarousel.css';
 
-function HomePage({ data }) {
+function MultiItemCarousel({ data }) {
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 4,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
   return (
-    <div>
+    <Carousel responsive={responsive}>
       {data.map((element) => (
         <div key={element.id}>
-          <p>ID: {element.id}</p>
-          <p>Title: {element.title}</p>
-          <p>Price: {element.price}</p>
-          <p>Description: {element.description}</p>
-          <p>Category: {element.category}</p>
-          <img src={element.image} alt={element.title} />
-          <p>Rating: {element.rating.rate} (Count: {element.rating.count})</p>
+          <div className="card hoverable-card">
+            <img src={element.image} className="card-img-top" alt={element.title} />
+            <div className="card-body">
+              <h6 className="card-title">{element.title}</h6>
+              <p>Rating: {element.rating.rate}</p>
+              <p>Price: ${element.price}</p>
+            </div>
+            <div className="alternate-content">
+              <h5>{element.title}</h5>
+              <p>
+                {Array.from({ length: element.rating.rate }, (_, index) => (
+                  <i key={index} className="bi bi-star text-warning"></i>
+                ))}
+              </p>
+              <p className="details">{element.description}</p>
+            </div>
+          </div>
         </div>
       ))}
-    </div>
+    </Carousel>
   );
 }
 
-export async function getStaticProps() {
-  // Fetch data from an external API (e.g., fakestoreapi)
-  const res = await fetch('https://fakestoreapi.com/products');
-  const data = await res.json();
+function HomePage() {
+    const [data, setData] = useState([]);
 
-  return {
-    props: { data },
-  };
-}
+    useEffect(() => {
+      // Fetch your data here
+      fetch('https://fakestoreapi.com/products')
+        .then((response) => response.json())
+        .then((apiData) => setData(apiData))
+        .catch((error) => console.error('Error fetching data:', error));
+    }, []);
 
-export default HomePage;
+    return (
+      <div>
+        <h1>Multi-Item Carousel</h1>
+        <MultiItemCarousel data={data} />
+      </div>
+    );
+  }
+
+  export default HomePage;
