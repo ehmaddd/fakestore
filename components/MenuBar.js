73adccaafbb3961fa726689eef7c747e38154693
ehmaddd <ehmaddd@gmail.com>
2023-10-24 22:51:48 +0500

    Adjust categories link

--- components/MenuBar.js
+++ components/MenuBar.js
@@ -8,14 +8,11 @@ const MenuBar = () => {
   }, []);
   return (
     <div class="dropdown">
-      <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-        Dropdown button
-      </button>
-      <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-        <li><a class="dropdown-item" href="#">
-
-            </a>
-        </li>
+      <a class="btn btn-light dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        Categories
+      </a>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="#">Action</a></li>
         <li><a class="dropdown-item" href="#">Another action</a></li>
         <li><a class="dropdown-item" href="#">Something else here</a></li>
       </ul>
