f9e933331779fbb04909f272052ca10394d6caef
ehmaddd <ehmaddd@gmail.com>
2023-10-24 23:08:11 +0500

    Display sub-menu items

--- components/MenuBar.js
+++ components/MenuBar.js
@@ -1,20 +1,53 @@
-import { React, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 const MenuBar = () => {
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
+
   return (
-    <div class="dropdown">
-      <a class="btn btn-light dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+    <div className="dropdown">
+      <a className="btn dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
         Categories
       </a>
-      <ul class="dropdown-menu">
-        <li><a class="dropdown-item" href="#">Action</a></li>
-        <li><a class="dropdown-item" href="#">Another action</a></li>
-        <li><a class="dropdown-item" href="#">Something else here</a></li>
+      <ul className="dropdown-menu">
+        <li>
+          <div className="btn-group dropend">
+            <button type="button" className="btn dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+              First Link
+            </button>
+            <ul className="dropdown-menu">
+              <li><a className="dropdown-item" href="#">Menu item</a></li>
+              <li><a className="dropdown-item" href="#">Menu item</a></li>
+              <li><a className="dropdown-item" href="#">Menu item</a></li>
+            </ul>
+          </div>
+        </li>
+        <li>
+          <div className="btn-group dropend">
+            <button type="button" className="btn dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+              Second Link
+            </button>
+            <ul className="dropdown-menu">
+              <li><a className="dropdown-item" href="#">Menu item</a></li>
+              <li><a className="dropdown-item" href="#">Menu item</a></li>
+              <li><a className="dropdown-item" href="#">Menu item</a></li>
+            </ul>
+          </div>
+        </li>
+        <li>
+          <div className="btn-group dropend">
+            <button type="button" className="btn dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+              Third Link
+            </button>
+            <ul className="dropdown-menu">
+              <li><a className="dropdown-item" href="#">Menu item</a></li>
+              <li><a className="dropdown-item" href="#">Menu item</a></li>
+              <li><a className="dropdown-item" href="#">Menu item</a></li>
+            </ul>
+          </div>
+        </li>
       </ul>
     </div>
   );
